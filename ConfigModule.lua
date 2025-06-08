a2de5dd1d657bceee0623512e1147d5694cc5f43
ScienceSequence <ratskoe@bk.ru>
2025-06-08 20:18:36 +0800

    Create ConfigModule.lua

---
+++ ConfigModule.lua
@@ -0,0 +1,24 @@
+local GunConfig = {
+	
+	RailGun = {
+		
+		Damage = 20,
+		FireRate = .1,
+		Ammo = 30,
+		MaxAmmo = 30,
+		ReloadTime = 3,
+		Range = 300,
+		
+	},
+	
+	Rifle = {
+		Damage = 95,
+		FireRate = 1.2,
+		Ammo = 6,
+		MaxAmmo = 6,
+		ReloadTime = 3,
+		Range = 500,
+	}
+	
+}
+return GunConfig 
